4b4a51c25472bc33057d643067e4d269e7ac81b5
gabri <gabrigioi@hotmail.com>
2023-01-30 18:59:54 +0100

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='toolbox',
-    version='0.0.15',
+    version='0.0.16',
     author='Alex Valle | Gabriele Berruti',
     author_email='alexvalle75@gmail.com',
     description='Useful package for analyze, manipulate and plot time series',
--- tstools/analysis.py
+++ tstools/analysis.py
@@ -423,7 +423,7 @@ class SmoothingResult():
         )
 
 
-    def plot_smoothed(self, title=""):
+    def plot_smoothed(self):
         """
         Description
         -----------
